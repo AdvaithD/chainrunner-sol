daf89377c93a7bdc1b927a7723fc3fe4db696cbf
AdvaithD <advaith@chainist.com>
2022-01-22 14:39:17 -0800

    deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const chainrunner = await ethers.getContractFactory("Chainrunner");
+  const Chainrunner = await chainrunner.deploy();
 
-  await greeter.deployed();
+  await Chainrunner.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Chainrunner deployed to:", Chainrunner.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
